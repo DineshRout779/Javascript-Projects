6014d10eeab979bdc43bba1a28bc3de05441362c
DineshRout779 <rddinesh779@gmail.com>
2020-11-02 20:58:05 +0530

    project finished

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -1,20 +1,29 @@
-let date = new Date();
-let h = date.getHours();
-let m = date.getMinutes();
-let s = date.getMilliseconds();
-let session = "AM";
 let clock = document.querySelector(".clock");
 
-function showTime(){
+function showTime() {
+  let date = new Date();
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  let session = "AM";
 
-    if(h == 0){
-        h = 12;
-    }
+  if (h == 0) {
+    h = 12;
+  }
 
-    if(h > 12){
-        h -= 12;
-        session = "PM";
-    }
+  if (h > 12) {
+    h -= 12;
+    session = "PM";
+  }
 
-    
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  let time = h + ":" + m + ":" + s + ":" + session;
+
+  clock.textContent = time;
+  setTimeout(showTime, 1000)
 }
+
+showTime();
