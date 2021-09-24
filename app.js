a844663684daf32d16a3ea8cb9089f8ea43c6e68
Dinesh Rout <49337110+DineshRout779@users.noreply.github.com>
2021-09-24 14:29:05 +0530

    fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $(".project__item").slice(0, 6).show();
   $(".load").click(function (e) {
     e.preventDefault();
-    $(".project__item").slice(6, 9).slideToggle();
+    $(".project__item").slice(6, 10).slideToggle();
     $(this).text($(this).text() === "Load Less" ? "Load More" : "Load Less");
   });
 });
